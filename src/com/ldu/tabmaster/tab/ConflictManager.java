25e02cbb7bf9b8b49a4b362ab29dc5dbdb939c28
Maxym Naumchyk <max.naumch@gmail.com>
2018-05-15 14:20:34 +0300

    New fns to add conflicts

--- src/com/ldu/tabmaster/tab/ConflictManager.java
+++ src/com/ldu/tabmaster/tab/ConflictManager.java
@@ -1,6 +1,7 @@
 package com.ldu.tabmaster.tab;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.ldu.tabmaster.tab.elements.*;
@@ -59,5 +60,79 @@ public class ConflictManager {
         return teamWithTeams.get(team);
     }
 
-    // TODO adding new conflicts
+    /**
+     * Add new conflict between judge and team.
+     * 
+     * @param t
+     *            Team that is in conflict.
+     * @param j
+     *            Judge that is in conflict.
+     * @exception NullPointerException
+     *                Raises when either team or judge argument is null.
+     */
+    public void addTeamConflictWithJudge(Team t, Judge j)
+            throws NullPointerException {
+        // Validate arguments.
+        if (t == null)
+            throw new NullPointerException("Team argument cannot be null");
+        if (j == null)
+            throw new NullPointerException("Judge argument cannot be null");
+
+        // Check whether conflict set for this team exists.
+        Set<Judge> setJ = teamConflictsWithJudges(t);
+        if (setJ == null) {
+            // Create new team entry.
+            setJ = new HashSet<Judge>();
+            teamWithJudges.put(t, setJ);
+        }
+
+        setJ.add(j);
+
+        // Check whether set for this judge exists.
+        Set<Team> set = judgeConflicts(j);
+        if (set == null) {
+            // Create new team entry.
+            set = new HashSet<Team>();
+            judgeWithTeams.put(j, set);
+        }
+
+        set.add(t);
+    }
+
+    /**
+     * Add conflict between two teams. The result is the same if 'a' and 'b'
+     * argument get swapped.
+     * 
+     * @param a
+     *            First team in conflict.
+     * @param b
+     *            Second team in conflict.
+     * @throws NullPointerException
+     *             Raises when either team is null.
+     */
+    public void addTeamConflictWithTeam(Team a, Team b)
+            throws NullPointerException {
+        // Validate arguments.
+        if (a == null)
+            throw new NullPointerException("First team cannot be null");
+        if (b == null)
+            throw new NullPointerException("Second team cannot be null");
+
+        Set<Team> setA = teamConflictsWithTeams(a);
+        Set<Team> setB = teamConflictsWithTeams(b);
+
+        if (setA == null) {
+            // Create new team entry.
+            setA = new HashSet<Team>();
+            teamWithTeams.put(a, setA);
+        }
+        if (setB == null) {
+            // Create new team entry.
+            setA = new HashSet<Team>();
+            teamWithTeams.put(b, setB);
+        }
+
+        setA.add(b);
+        setB.add(a);
+    }
 }
