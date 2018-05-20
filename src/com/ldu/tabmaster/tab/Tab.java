c7102b4c9b38fbb6988e29fed95fb454fb3ac8ac
Maxym Naumchyk <max.naumch@gmail.com>
2018-05-20 12:29:37 +0300

    New unimplemented ctor

--- src/com/ldu/tabmaster/tab/Tab.java
+++ src/com/ldu/tabmaster/tab/Tab.java
@@ -2,7 +2,13 @@ package com.ldu.tabmaster.tab;
 
 import com.ldu.tabmaster.tab.elements.Room;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Tab {
 
     private Room rooms[];
+    
+    public Tab(Room rooms[]) {
+        throw new NotImplementedException();
+    }
 }
